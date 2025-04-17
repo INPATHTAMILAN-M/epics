78ca625caffba2a9329ccd9a096eaebaa06486a5
Sanjesh2254 <rsanjesh64@gmail.com>
2025-04-17 10:24:48 +0530

    change db

--- epic/settings.py
+++ epic/settings.py
@@ -76,28 +76,28 @@ WSGI_APPLICATION = 'epic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-
-#    'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'EPICS',
-#         'USER': 'root',
-#         'PASSWORD': 'KIT4compass!!!',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#         'OPTIONS': {
-#         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#         },
-        
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EPICS',
+        'USER': 'root',
+        'PASSWORD': 'KIT4compass!!!',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+        
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
